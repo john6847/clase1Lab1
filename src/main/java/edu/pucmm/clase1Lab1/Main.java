cab3fc1458e1eb3f6223af50f9f2e6c34188b1ad
john <20131256@ce.pucmm.edu.do>
2017-05-20 08:45:56 -0400

    First one

---
+++ src/main/java/edu/pucmm/clase1Lab1/Main.java
@@ -0,0 +1,11 @@
+package edu.pucmm.clase1Lab1;
+
+/**
+ * Created by john on 20/05/17.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
